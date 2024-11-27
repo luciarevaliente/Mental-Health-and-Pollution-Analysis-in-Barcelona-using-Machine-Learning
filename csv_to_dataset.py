d5c305dfb646685a2654dc1a045282d72ea5883b
luciarevaliente <1668160@uab.cat>
2024-11-27 18:23:09 +0100

    Update

--- csv_to_dataset.py
+++ csv_to_dataset.py
@@ -5,4 +5,10 @@ DATASET = "CitieSHealth_BCN_DATA_PanelStudy_20220414.csv"
 
 # CODI
 df = pd.read_csv(DATASET)
-print(head(df))
+# print(df.head())
+
+caracteristiques = df.columns
+print(f'CARACTERÍSTIQUES: {caracteristiques}')
+
+instancies = df.shape[0]
+print(f'Nº instàncies: {instancies}')
