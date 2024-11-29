94c58865ccf7d43603632f0852d4baa15d98e83a
luciarevaliente <1668160@uab.cat>
2024-11-29 21:58:03 +0100

    update csv_to_dataset

--- csv_to_dataset.py
+++ csv_to_dataset.py
@@ -13,10 +13,11 @@ if __name__=="__main__":
 
     # Dimensions
     row, col = df.shape
-    print(f'\nLes dimensions del dataset són {row} x {col}.')
+    print(f'\nLes dimensions del dataset són {row}x{col}.')
 
     # Valors null
     valors_null = df.isnull()
-    columnes_null = valors_null.any()
-    print(f'\nHi ha {len(df.columns[columnes_null])} columnes amb valors null.')
-
+    col_null = valors_null.any()
+    print(f'\nHi ha {len(df.columns[col_null])} columnes amb valors null.')
+    print(f'\nHi ha {len(df[valors_null])} valors null en el dataset.')
+    
