45ea88a152f11231acf6fdb7052d3fbcd46581f0
luciarevaliente <1668160@uab.cat>
2024-11-29 21:45:21 +0100

    update csv_to_dataset

--- csv_to_dataset.py
+++ csv_to_dataset.py
@@ -3,13 +3,20 @@ import pandas as pd
 # VARIABLES
 DATASET = "CitieSHealth_BCN_DATA_PanelStudy_20220414.csv"
 
-# CODI
+# IMPORTACIÓ DATASET
 df = pd.read_csv(DATASET)
 # print(df.head())
 
+# ANÀLISI CONTINGUT
+if __name__=="__main__":
+    print(f'\nDataset: {DATASET}')
 
-caracteristiques = df.columns
-print(f'CARACTERÍSTIQUES: {caracteristiques}')
+    # Dimensions
+    row, col = df.shape
+    print(f'\nLes dimensions del dataset són {row} x {col}.')
 
-instancies = df.shape[0]
-# print(f'Nº instàncies: {instancies}')
+    # Valors null
+    valors_null = df.isnull()
+    columnes_null = valors_null.any()
+    print(f'\nHi ha {len(df.columns[columnes_null])} columnes amb valors null.')
+
