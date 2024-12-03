ce0e4265da14818cd27e6251da5f90ba325af43e
luciarevaliente <1668160@uab.cat>
2024-12-03 13:14:22 +0100

    data_cleaning

--- scripts/03_data_cleaning.py
+++ scripts/03_data_cleaning.py
@@ -36,19 +36,71 @@ for column in data.columns:
 res = data.drop_duplicates(inplace=True)
 print(f'Hi havia {res} duplicats\n')
 
+# CONVERTIR TIPUS DE DADES INCORRECTES ############################################################################
+# for i, tipo in data.dtypes.items():
+#     print(i, tipo)
+
+col_date_docu = ['date_all']
+col_int_docu = ['occurrence_mental', 'stroop_test', 'occurrence_stroop', 'correct', 'response_duration_ms', 'start_day',
+            'start_month', 'start_year', 'start_hour', 'end_day', 'end_month', 'end_year', 'end_hour', 
+            'Totaltime', 'Totaltime_estimated', 'Houron', 'Houroff', 'age_yrs', 'yearbirth', 'hour_gps', 'sec_noise55_day',
+            'sec_noise65_day', 'sec_greenblue_day', 'hours_noise_55_day', 'hours_noise_65_day', 'hours_greenblue_day', 'precip_12h_binary',
+            'precip_24h_binary']
+col_str_docu = ['dayoftheweek', 'mentalhealth_survey', 'bienestar', 'energia', 'estres', 'sueno', 'horasfuera', 'ordenador', 
+           'dieta', 'alcohol', 'drogas', 'enfermo', 'otrofactor', 'district', 'education', 'access_greenbluespaces_300mbuff',
+           'smoke', 'psycho', 'gender']
+
+col_date_real = ['date_all']
+col_hour_real = ['Houron', 'Houroff']
+col_int_real = ['occurrence_mental', 'occurrence_stroop', 'correct', 'response_duration_ms', 'start_day',
+            'start_month', 'start_year', 'start_hour', 'end_day', 'end_month', 'end_year', 'end_hour', 
+            'Totaltime', 'Houron', 'Houroff', 'age_yrs', 'yearbirth', 'hour_gps', 'sec_noise55_day',
+            'sec_noise65_day', 'sec_greenblue_day', 'hours_noise_55_day', 'hours_noise_65_day', 'hours_greenblue_day', 'precip_12h_binary',
+            'precip_24h_binary']
+col_str_real = ['dayoftheweek', 'mentalhealth_survey', 'bienestar', 'energia', 'estres', 'sueno', 'horasfuera', 'ordenador', 
+           'dieta', 'alcohol', 'drogas', 'enfermo', 'otrofactor', 'district', 'education', 'access_greenbluespaces_300mbuff',
+           'smoke', 'psycho', 'gender', 'stroop_test', 'Totaltime_estimated']
+
+# print(data['stroop_test'])
+# print(data['Totaltime_estimated'])
+# print(data['Houron'])
+# print(data['Houroff'])
+
+# Convertir les columnes de dates a tipus datetime
+for col in col_date_real:
+    data[col] = pd.to_datetime(data[col], errors='coerce')  # 'coerce' per gestionar errors
+
+# Convertir les columnes de hores a Format HH:MM
+for col in col_hour_real:
+    data[col] = pd.to_timedelta(data[col], errors='coerce')
+    
+# Convertir les columnes d'enteres a tipus numèric
+for col in col_int_real:
+    data[col] = pd.to_numeric(data[col], errors='coerce')  # 'coerce' per substituir errors amb NaN
+
+# Les columnes de tipus string (ja han de ser strings, però es poden netejar)
+for col in col_str_real:
+    data[col] = data[col].astype(str)  # Assegura't que són strings
+    data[col] = data[col].str.lower().str.strip()  #Estàndard
+
+# Comprovar si hi ha NaN en tot el DataFrame
+nan_check = data.isna().any().any()  # Retorna True si hi ha qualsevol NaN al DataFrame
+
+if nan_check:
+    print("Hi ha valors NaN al DataFrame.")
+    # Mostrar totes les files amb almenys un NaN
+    nan_per_column = data.isna().sum()
+    for i, v in nan_per_column.items():
+        if v!=0:
+            print(i,v)
+    # print(nan_per_column)
+else:
+    print("No hi ha valors NaN al DataFrame.")
+
 # CORREGIR ERRORS TIPOGRÀFICS ####################################################################################
 # print(data.select_dtypes(include=['object', 'string']).columns)
 for column in data.select_dtypes(include=['object', 'string']).columns:
     data[column] = data[column].str.lower().str.strip()  # Estandarditza
 
-# CONVERTIR TIPUS DE DADES INCORRECTES ############################################################################
-for i, tipo in data.dtypes.items():
-    print(i, tipo)
-# canviar = 
-
-# NORMALITZACIÓ ###################################################################################################
-# dades categòriques
-# dades numèriques?
-
 # GUARDEM EL DATASET NET ##########################################################################################
-data.to_pickle(CLEANED_PICKLE_PATH)
+# data.to_pickle(CLEANED_PICKLE_PATH)
