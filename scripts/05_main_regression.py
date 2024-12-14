a7e895eb91e784a800fbac7bcf76c530ad413983
aranzazu2204 <aranzazumiguelezm@gmail.com>
2024-12-14 12:03:27 +0100

    regression

--- scripts/05_main_regression.py
+++ scripts/05_main_regression.py
@@ -4,6 +4,7 @@ from preprocess import preprocess
 from models_regression import RegressionModels, GRID_PARAMS, get_best_model
 import pandas as pd
 import os
+from sklearn.feature_selection import SelectKBest, f_regression
 
 # Configuración
 DATA_PATH = "data/cleaned_dataset.pkl"
@@ -17,39 +18,39 @@ def prepare_data(data_path, target_column):
 X_train, X_test, y_train, y_test = prepare_data(DATA_PATH, TARGET_COLUMN)
 
 # Evaluar modelos y calcular importancia de características
-def evaluate_models(model_types, X_train, y_train):
-    results = {}
-    for model_name in model_types:
-        print(f"Evaluando {model_name}...")
-        model_instance = RegressionModels(model_type=model_name)
-        param_grid = GRID_PARAMS.get(model_name, {})
-        best_model = get_best_model(model_name, model_instance.get_model(), param_grid,X_train, y_train,X_test, y_test)
-        best_model.fit(X_train, y_train)
+# def evaluate_models(model_types, X_train, y_train):
+#     results = {}
+#     for model_name in model_types:
+#         print(f"Evaluando {model_name}...")
+#         model_instance = RegressionModels(model_type=model_name)
+#         param_grid = GRID_PARAMS.get(model_name, {})
+#         best_model = get_best_model(model_name, model_instance.get_model(), param_grid,X_train, y_train,X_test, y_test)
+#         best_model.fit(X_train, y_train)
 
-        # Obtener importancia de características
-        if hasattr(best_model, "coef_"):  # Modelos lineales
-            coefficients = pd.DataFrame({
-                'Feature': X_train.columns,
-                'Coefficient': abs(best_model.coef_)
-            }).sort_values(by='Coefficient', ascending=False)
-            results[model_name] = coefficients
+#         # Obtener importancia de características
+#         if hasattr(best_model, "coef_"):  # Modelos lineales
+#             coefficients = pd.DataFrame({
+#                 'Feature': X_train.columns,
+#                 'Coefficient': abs(best_model.coef_)
+#             }).sort_values(by='Coefficient', ascending=False)
+#             results[model_name] = coefficients
 
-        elif hasattr(best_model, "feature_importances_"):  # Modelos de árboles
-            importances = pd.DataFrame({
-                'Feature': X_train.columns,
-                'Importance': best_model.feature_importances_
-            }).sort_values(by='Importance', ascending=False)
-            results[model_name] = importances
+#         elif hasattr(best_model, "feature_importances_"):  # Modelos de árboles
+#             importances = pd.DataFrame({
+#                 'Feature': X_train.columns,
+#                 'Importance': best_model.feature_importances_
+#             }).sort_values(by='Importance', ascending=False)
+#             results[model_name] = importances
 
-        else:  # Modelos sin coeficientes directos
-            from sklearn.inspection import permutation_importance
-            perm_importance = permutation_importance(best_model, X_train, y_train, random_state=42)
-            importances = pd.DataFrame({
-                'Feature': X_train.columns,
-                'Importance': perm_importance.importances_mean
-            }).sort_values(by='Importance', ascending=False)
-            results[model_name] = importances
-    return results
+#         else:  # Modelos sin coeficientes directos
+#             from sklearn.inspection import permutation_importance
+#             perm_importance = permutation_importance(best_model, X_train, y_train, random_state=42)
+#             importances = pd.DataFrame({
+#                 'Feature': X_train.columns,
+#                 'Importance': perm_importance.importances_mean
+#             }).sort_values(by='Importance', ascending=False)
+#             results[model_name] = importances
+#     return results
 
 # Guardar resultados
 def save_results(results, output_dir="data/regression/results"):
@@ -58,40 +59,48 @@ def save_results(results, output_dir="data/regression/results"):
         importance_df.to_excel(os.path.join(output_dir, f"{model_name}_importance.xlsx"), index=False)
         print(f"Resultados guardados para {model_name} en {output_dir}")
 
-# Extraer las 10 características más importantes comunes entre los métodos
-def extract_top_common_features(results, top_n=50):
-    """
-    Extrae las características comunes más importantes de los resultados.
+# # Extraer las 10 características más importantes comunes entre los métodos
+# def extract_top_common_features(results, top_n=50):
+#     """
+#     Extrae las características comunes más importantes de los resultados.
     
-    Args:
-        results (dict): Diccionario con las importancias de las características por modelo.
-        top_n (int): Número de características principales a considerar de cada modelo.
+#     Args:
+#         results (dict): Diccionario con las importancias de las características por modelo.
+#         top_n (int): Número de características principales a considerar de cada modelo.
 
-    Returns:
-        set: Conjunto de características comunes entre los modelos.
-    """
-    # Lista para almacenar conjuntos de las características más importantes
-    top_features_sets = []
+#     Returns:
+#         set: Conjunto de características comunes entre los modelos.
+#     """
+#     # Lista para almacenar conjuntos de las características más importantes
+#     top_features_sets = []
     
-    for model_name, importance_df in results.items():
-        # Tomar las top_n características más importantes de cada modelo
-        top_features = set(importance_df.head(top_n)['Feature'])
-        top_features_sets.append(top_features)
+#     for model_name, importance_df in results.items():
+#         # Tomar las top_n características más importantes de cada modelo
+#         top_features = set(importance_df.head(top_n)['Feature'])
+#         top_features_sets.append(top_features)
     
-    # Intersección de las características principales de todos los modelos
-    common_features = set.intersection(*top_features_sets)
-    return common_features
+#     # Intersección de las características principales de todos los modelos
+#     common_features = set.intersection(*top_features_sets)
+#     return common_features
 
+def select_k_best_features(X_train, y_train, k):
+    """
+    Selecciona las k mejores características usando SelectKBest.
+    """
+    selector = SelectKBest(score_func=f_regression, k=k)
+    X_train_selected = selector.fit_transform(X_train, y_train)
+    selected_features = X_train.columns[selector.get_support()]
+    return X_train_selected, selected_features, selector
 
 # Ejecución principal
 if __name__ == "__main__":
     model_types = ["ridge", "lasso", "random_forest", "gradient_boosting", "xgboost", "polynomial_regression"]
-    results = evaluate_models(model_types, X_train, y_train)
+    # results = evaluate_models(model_types, X_train, y_train)
     # save_results(results)
-    print("Evaluación completada.")
+    # print("Evaluación completada.")
 
     # Extraer las 10 características más importantes comunes
-    common_features = extract_top_common_features(results, top_n=10)
+    common_features = select_k_best_features(X_train,y_train, 20)
     print("Las 10 características más importantes comunes entre los modelos son:")
     print(common_features)
     print("Evaluación completada.")
--- scripts_desus/prova.py
+++ scripts_desus/prova.py
@@ -1,41 +1,59 @@
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# import pandas as pd
+# from sklearn.preprocessing import MinMaxScaler
+# import plotly.graph_objects as go
+# from sklearn.model_selection import train_test_split
+# from sklearn.ensemble import RandomForestRegressor
+
+# cleaned_dataset_path = 'data/cleaned_dataset.pkl'
+# codif_dataset_path = 'data/codif_dataset.pkl'
+
+# cleaned_dataset = pd.read_pickle(cleaned_dataset_path)
+# data = pd.read_pickle(codif_dataset_path)
+
+# # Verificar si la columna 'estres' está en el dataset limpio
+# if 'estres' in cleaned_dataset.columns:
+#     # Añadir la columna 'estres' al dataset codificado
+#     if cleaned_dataset.shape[0] == data.shape[0]:
+#         data['estres'] = cleaned_dataset['estres']
+#         print("Columna 'estres' añadida correctamente.")
+# def load_and_prepare_data(data, features, target_column):
+#     """Carga y prepara los datos, seleccionando las características y el target."""
+    
+#     X = data[features]
+#     y = data[target_column]
+#     return train_test_split(X, y, test_size=0.2, random_state=42)
+
+# X_train, y_train, X_test, y_test = load_and_prepare_data(data, ['ordenador', 'otrofactor', 'dayoftheweek', 'bienestar','covid_motor','drogas',], 'estres')
+
+# rf = RandomForestRegressor()
+# rf.fit(X_train, y_train)
+# fig = go.Figure(go.Bar(
+#             x=rf.feature_importances_,
+#             y=X_train.columns,
+#             orientation='h', marker_color='steelblue'))
+# fig.update_layout(template='plotly_dark', title='<b>Estimating feature importance through the Random Forest model', title_x=0.5, 
+#                  xaxis_title="Feature importance", yaxis_title='Feature')
+
+# fig.show()
+
 import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 
+# Simulación de una curva de aprendizaje ideal
+iterations = np.arange(1, 21)
+train_error = 1 / iterations + 0.2  # Error de entrenamiento disminuye rápido
+test_error = 1 / (iterations + 3) + 0.5  # Error de prueba disminuye más lento y se estabiliza
 
-
-data = 'data/codif_dataset.pkl'
-dataset = pd.read_pickle(data)
-# Seleccionar las columnas relevantes (variables predictoras y la variable objetivo)
-variables_predictoras = ["bienestar", "ordenador", "otrofactor"]  # Reemplaza con tus variables
-import pandas as pd
-
-# Cargar el dataset
-cleaned_dataset_path = 'data/cleaned_dataset.pkl'
-codif_dataset_path = 'data/codif_dataset.pkl'
-
-cleaned_dataset = pd.read_pickle(cleaned_dataset_path)
-codif_dataset = pd.read_pickle(codif_dataset_path)
-
-# Verificar si la columna 'estres' está en el dataset limpio
-if 'estres' in cleaned_dataset.columns:
-    # Añadir la columna 'estres' al dataset codificado
-    if cleaned_dataset.shape[0] == codif_dataset.shape[0]:
-        codif_dataset['estres'] = cleaned_dataset['estres']
-        print("Columna 'estres' añadida correctamente.")
-    else:
-        print("Error: Los datasets no tienen el mismo número de filas.")
-
-# Verificar si 'estres' está en las columnas
-
-scaler = MinMaxScaler()
-codif_dataset['estres_scaled'] = scaler.fit_transform(codif_dataset[['estres']])
-for var in variables_predictoras:
-    plt.figure(figsize=(6, 4))
-    sns.scatterplot(x=codif_dataset[var], y=codif_dataset["estres"])
-    plt.title(f"Relación entre {var} y estrés")
-    plt.xlabel(var)
-    plt.ylabel("Estrés")
-    plt.grid(True)
-    plt.show()
+# Gráfico de la curva de aprendizaje
+plt.figure(figsize=(8, 6))
+plt.plot(iterations, train_error, label='Error de Entrenamiento', marker='o')
+plt.plot(iterations, test_error, label='Error de Prueba', marker='o')
+plt.axhline(y=0, color='gray', linestyle='--', linewidth=0.7)
+plt.title('Curva de Aprendizaje Ideal', fontsize=14)
+plt.xlabel('Número de Iteraciones (o Complejidad del Modelo)', fontsize=12)
+plt.ylabel('Error', fontsize=12)
+plt.legend(fontsize=12)
+plt.grid(alpha=0.3)
+plt.show()
