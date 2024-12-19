358864e1f477839504df3b5bf6d8a75d2e695a62
aranzazu2204 <aranzazumiguelezm@gmail.com>
2024-12-19 15:00:44 +0100

    regression

--- scripts/05_main_regression.py
+++ scripts/05_main_regression.py
@@ -18,39 +18,38 @@ def prepare_data(data, target_column):
 X_train, X_test, y_train, y_test = prepare_data(pd.read_pickle(DATA_PATH), TARGET_COLUMN)
 
 # Evaluar modelos y calcular importancia de características
-# def evaluate_models(model_types, X_train, y_train):
-#     results = {}
-#     for model_name in model_types:
-#         print(f"Evaluando {model_name}...")
-#         model_instance = RegressionModels(model_type=model_name)
-#         param_grid = GRID_PARAMS.get(model_name, {})
-#         best_model = get_best_model(model_name, model_instance.get_model(), param_grid,X_train, y_train,X_test, y_test)
-#         best_model.fit(X_train, y_train)
+def evaluate_models(model_types, X_train, y_train):
+     results = {}
+     for model_name in model_types:
+         print(f"Evaluando {model_name}...")
+         model_instance = RegressionModels(model_type=model_name)
+         param_grid = GRID_PARAMS.get(model_name, {})
+         best_model = get_best_model(model_name, model_instance.get_model(), param_grid,X_train, y_train,X_test, y_test)
+         best_model.fit(X_train, y_train)
+         # Obtener importancia de características
+         if hasattr(best_model, "coef_"):  # Modelos lineales
+             coefficients = pd.DataFrame({
+                 'Feature': X_train.columns,
+                 'Coefficient': abs(best_model.coef_)
+             }).sort_values(by='Coefficient', ascending=False)
+             results[model_name] = coefficients
 
-#         # Obtener importancia de características
-#         if hasattr(best_model, "coef_"):  # Modelos lineales
-#             coefficients = pd.DataFrame({
-#                 'Feature': X_train.columns,
-#                 'Coefficient': abs(best_model.coef_)
-#             }).sort_values(by='Coefficient', ascending=False)
-#             results[model_name] = coefficients
+         elif hasattr(best_model, "feature_importances_"):  # Modelos de árboles
+             importances = pd.DataFrame({
+                 'Feature': X_train.columns,
+                 'Importance': best_model.feature_importances_
+             }).sort_values(by='Importance', ascending=False)
+             results[model_name] = importances
 
-#         elif hasattr(best_model, "feature_importances_"):  # Modelos de árboles
-#             importances = pd.DataFrame({
-#                 'Feature': X_train.columns,
-#                 'Importance': best_model.feature_importances_
-#             }).sort_values(by='Importance', ascending=False)
-#             results[model_name] = importances
-
-#         else:  # Modelos sin coeficientes directos
-#             from sklearn.inspection import permutation_importance
-#             perm_importance = permutation_importance(best_model, X_train, y_train, random_state=42)
-#             importances = pd.DataFrame({
-#                 'Feature': X_train.columns,
-#                 'Importance': perm_importance.importances_mean
-#             }).sort_values(by='Importance', ascending=False)
-#             results[model_name] = importances
-#     return results
+         else:  # Modelos sin coeficientes directos
+             from sklearn.inspection import permutation_importance
+             perm_importance = permutation_importance(best_model, X_train, y_train, random_state=42)
+             importances = pd.DataFrame({
+                 'Feature': X_train.columns,
+                 'Importance': perm_importance.importances_mean
+             }).sort_values(by='Importance', ascending=False)
+             results[model_name] = importances
+     return results
 
 # Guardar resultados
 def save_results(results, output_dir="data/regression/results"):
@@ -94,9 +93,9 @@ def select_k_best_features(X_train, y_train, k):
 
 # Ejecución principal
 if __name__ == "__main__":
-    model_types = ["ridge", "lasso", "random_forest", "gradient_boosting", "xgboost", "polynomial_regression"]
-    # results = evaluate_models(model_types, X_train, y_train)
-    # save_results(results)
+    model_types = [ "random_forest", "gradient_boosting", "xgboost"]
+    results = evaluate_models(model_types, X_train, y_train)
+    save_results(results)
     # print("Evaluación completada.")
 
     # Extraer las 10 características más importantes comunes
