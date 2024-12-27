b8356dfb5d9c505c65c61518fa3fe32a18130564
luciarevaliente <1668160@uab.cat>
2024-12-27 18:39:07 +0100

    retocs finals

--- scripts/03_data_cleaning.py
+++ scripts/03_data_cleaning.py
@@ -1,8 +1,8 @@
 """
-Script per natejar les dades de salut mental i contaminació de BCN
+Script per netejar les dades de salut mental i contaminació de BCN
 Creat per: Lucía Revaliente i Aránzazu Miguélez
 Data de creació: 03/12/24
-Descripció: Aquest script nateja les dades de salut mental i contaminació.
+Descripció: Aquest script neteja les dades de salut mental i contaminació.
 """
 # 03_data_cleaning.py
 # IMPORTACIÓ
@@ -10,21 +10,21 @@ import pandas as pd
 from sklearn.impute import KNNImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
-# VARIABLES CONSTANTES
+# VARIABLES CONSTANTS
 PICKLE_PATH = 'data/dataset.pkl'
 CLEANED_PICKLE_PATH = 'data/cleaned_dataset.pkl'
 k = 5
-ESBORRAR = ['date_all', 'year', 'month', 'hour', 'day', 'start_year','start_month', 'start_day', 'start_hour', 'end_year','end_month', 'end_day', 'end_hour', 'Houron', 'Houroff', # Les dades temporals de l'enquesta no aporten info, només per controlar dataset
+ESBORRAR = ['date_all', 'year', 'month', 'hour', 'day', 'start_year','start_month', 'start_day', 'start_hour', 'end_year','end_month', 'end_day', 'end_hour', 'Houron', 'Houroff', # Les dades temporals de l'enquesta no aporten informació, només per controlar el dataset
             'stroop_test', # Només pren un valor
             'yearbirth', # Ja tenim la variable age 
             'no2gps_12hx30','no2gps_24hx30','no2gps_12h','no2bcn_12h_x30','no2bcn_24h_x30','no2bcn_12h','no2gps_12h_x30','no2gps_24h_x30'  # Factors de 30
             ] 
 
-# FUNCIONES ###############################################################
-# Eliminar valores nulos
+# FUNCIONS ###############################################################
+# Eliminar valors nuls
 def filtrar_valors_null(dataset, k):
     data_cleaned = dataset.copy()
-    data_cleaned = data_cleaned.loc[:, data_cleaned.isnull().mean() < 0.5]  # Eliminar columnas con más del 50% de NaN
+    data_cleaned = data_cleaned.loc[:, data_cleaned.isnull().mean() < 0.5]  # Eliminar columnes amb més del 50% de NaN
 
     knn_imputer = KNNImputer(n_neighbors=k)
     data_imputed = knn_imputer.fit_transform(data_cleaned.select_dtypes(include=['number']))
@@ -35,11 +35,11 @@ def filtrar_valors_null(dataset, k):
         data_cleaned[column] = data_cleaned[column].fillna(mode_value)
     return data_cleaned
 
-# Eliminar duplicados
-def eliminar_duplicados(dataset):
+# Eliminar duplicats
+def eliminar_duplicats(dataset):
     return dataset.drop_duplicates()
 
-# Convertir tipos de datos
+# Convertir tipus de dades
 def convertir_tipus_de_dades(dataset, enter=[], caracter=[]):
     if enter:
         for col in enter:
@@ -52,7 +52,7 @@ def convertir_tipus_de_dades(dataset, enter=[], caracter=[]):
                 dataset[col] = dataset[col].astype(str)
     return dataset
 
-# Normalizar valores categóricos
+# Normalitzar valors categòrics
 def estandarditzar_valors_categorics(dataset, normalitzar=[]):
     for column in dataset.select_dtypes(include=['object', 'string']).columns:
         dataset[column] = dataset[column].str.lower().str.strip()
@@ -60,13 +60,13 @@ def estandarditzar_valors_categorics(dataset, normalitzar=[]):
         dataset[col] = dataset[col].round().astype('Int64')
     return dataset
 
-# Escalar datos numéricos
+# Escalar dades numèriques
 def escalar_dades(dataset, numeric_columns):
     scaler = StandardScaler()
     dataset[numeric_columns] = scaler.fit_transform(dataset[numeric_columns])
     return dataset
 
-# Codificar datos categóricos
+# Codificar dades categòriques
 def codificar_dades_categoriques(dataset, categorical_columns):
     encoder = OneHotEncoder(sparse_output=False, drop='first')
     encoded_data = encoder.fit_transform(dataset[categorical_columns])
@@ -76,34 +76,21 @@ def codificar_dades_categoriques(dataset, categorical_columns):
     dataset = pd.concat([dataset, encoded_df], axis=1)
     return dataset
 
-# PROCESAMIENTO ##########################################################
+# PROCESSAMENT ##########################################################
 if __name__=="__main__":
-    # Cargar datos
+    # Pas 0: Carregar dades i eliminar columnes innecessàries
     data = pd.read_pickle(PICKLE_PATH)
-    print(data.shape)
-    h = input('h')
-    # Eliminar columnas innecesarias
     data = data.drop(columns=ESBORRAR, errors='ignore')
-    print(data.shape)
-    h = input('h')
-    # # Conversión de formatos
-    # data['Hour] = pd.to_datetime(data['Houron'], format='%H:%M:%S', errors='coerce')
-    # data['Houroff'] = pd.to_datetime(data['Houroff'], format='%H:%M:%S', errors='coerce')
-    # data['Houron_hour'] = data['Houron'].dt.hour.astype('Int64')
-    # data['Houron_minute'] = data['Houron'].dt.minute.astype('Int64')
-    # data['Houroff_hour'] = data['Houroff'].dt.hour.astype('Int64')
-    # data['Houroff_minute'] = data['Houroff'].dt.minute.astype('Int64')
-    # data = data.drop(columns=['Houron', 'Houroff'], errors='ignore')on'
 
-    # Paso 1: Tratar valores nulos
+    # Pas 1: Tractar valors nuls
     cleaned_dataset = filtrar_valors_null(data, k)
     print(cleaned_dataset.shape)
-    h = input('h')
-    # Paso 2: Eliminar duplicados
-    cleaned_dataset = eliminar_duplicados(cleaned_dataset)
+
+    # Pas 2: Eliminar duplicats
+    cleaned_dataset = eliminar_duplicats(cleaned_dataset)
     print(cleaned_dataset.shape)
-    h = input('h')
-    # Paso 3: Convertir tipos de datos
+
+    # Pas 3: Convertir tipus de dades
     transform_to_int = ['occurrence_mental', 'occurrence_stroop', 'correct', 'response_duration_ms', 
                         'age_yrs', 'hour_gps', 'sec_noise55_day', 'sec_noise65_day', 'sec_greenblue_day',
                         'hours_noise_55_day', 'hours_noise_65_day', 'hours_greenblue_day', 'precip_12h_binary',
@@ -113,20 +100,17 @@ if __name__=="__main__":
                         'district', 'education', 'access_greenbluespaces_300mbuff', 'smoke', 'psycho', 'gender', 
                         'Totaltime_estimated'] 
     cleaned_dataset = convertir_tipus_de_dades(cleaned_dataset, enter=transform_to_int, caracter=transform_to_str)
-    print(cleaned_dataset.shape)
-    h = input('h')
-    # Paso 4: Normalizar valores categóricos
+
+    # Pas 4: Normalitzar valors categòrics
     cleaned_dataset = estandarditzar_valors_categorics(cleaned_dataset, ['bienestar', 'energia', 'estres', 'sueno'])
     print(cleaned_dataset.shape)
-    h = input('h')
 
-    
     # GUARDAR EL DATASET ######################################################
     cleaned_dataset.to_pickle(CLEANED_PICKLE_PATH)
 
     # Exportar a Excel
     try:
         cleaned_dataset.to_excel('data/cleaned_dataset.xlsx', index=False)
-        print(f"El archivo ha sido exportado a: 'data/cleaned_dataset.xlsx'")
+        print(f"L'arxiu ha estat exportat a: 'data/cleaned_dataset.xlsx'")
     except Exception as e:
         print(f"Error: {e}")
