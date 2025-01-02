93a1a835b06c6486105f3adfa63ac567b2afe1fb
aranzazu2204 <aranzazumiguelezm@gmail.com>
2025-01-02 22:05:54 +0100

    regression

--- scripts/preparation_regression.py
+++ scripts/preparation_regression.py
@@ -34,33 +34,3 @@ def separacio_train_test(data, target_columns):
 
     return X_train, X_test, y_train, y_test
 
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-# Carga el dataset (sustituye 'your_dataset.csv' con el nombre del archivo)
-data = pd.read_csv('data\CitieSHealth_BCN_DATA_PanelStudy_20220414.csv')
-
-# Especifica la columna target
-target_column = 'estres'  # Sustituye con el nombre de tu columna target
-
-# Verifica los primeros valores
-print(data[target_column].head())
-
-# Visualización con Matplotlib
-plt.figure(figsize=(8, 6))
-plt.hist(data[target_column], bins=30, edgecolor='black', alpha=0.7)
-plt.title('Distribució de la variable estres', fontsize=16)
-plt.xlabel('Valor del target', fontsize=12)
-plt.ylabel('Frecuencia', fontsize=12)
-plt.grid(axis='y', linestyle='--', alpha=0.7)
-plt.show()
-
-# Visualización con Seaborn
-plt.figure(figsize=(8, 6))
-sns.histplot(data[target_column], bins=30, kde=True, color='blue', alpha=0.6)
-plt.title('Distribución de la variable objetivo con KDE', fontsize=16)
-plt.xlabel('Valor del target', fontsize=12)
-plt.ylabel('Frecuencia', fontsize=12)
-plt.grid(axis='y', linestyle='--', alpha=0.7)
-plt.show()
