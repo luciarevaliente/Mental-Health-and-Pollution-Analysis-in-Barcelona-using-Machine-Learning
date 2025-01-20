2f377da5a68d7080118650dc633b5fd44c94fef6
luciarevaliente <1668160@uab.cat>
2025-01-20 20:01:00 +0100

    main

---
+++ scripts/main.py
@@ -0,0 +1,9 @@
+"""
+main.py
+Script principal per processar les dades de salut mental i contaminació de BCN
+Creat per: Lucía Revaliente 
+Data de creació: 20/01/25
+Descripció: Aquest script neteja les dades de salut mental i contaminació, les analitza, empra regressió i clústering. Finalment, crea un perfil de l'estrès poblacional. 
+"""
+
+def main():
